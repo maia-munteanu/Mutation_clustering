e89a9a230fefc7970d593e8962ec3189738f2de8
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2023-05-09 17:59:17 +0200

    Update Extractor.py

--- Extractor.py
+++ Extractor.py
@@ -3,8 +3,10 @@ import os
 import sys
 from SigProfilerExtractor import sigpro as sig
 
+input = os.getcwd() + "/VCFs/
+
 def main_function():
-      sig.sigProfilerExtractor("vcf", "./VCFs/", "./VCFs/", reference_genome="GRCh37", opportunity_genome = "GRCh37", context_type = "96", exome = False, 
+      sig.sigProfilerExtractor("vcf", "Signatures", input, reference_genome="GRCh37", opportunity_genome = "GRCh37", context_type = "96", exome = False, 
                                minimum_signatures=1, maximum_signatures=2, nmf_replicates=100, resample = True, batch_size=1, cpu=1)
 if __name__=="__main__":
    main_function()
