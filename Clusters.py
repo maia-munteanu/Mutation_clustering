262f1b99e21035e2a80b4afc69ee2f7f094636f1
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2023-05-10 12:00:12 +0200

    Update Clusters.py

--- Clusters.py
+++ Clusters.py
@@ -5,15 +5,15 @@ from SigProfilerSimulator import SigProfilerSimulator as sigSim
 from SigProfilerClusters import SigProfilerClusters as hp
 
 project="Test"
-project_path="./VCFs/"
+path="./VCFs/"
 genome="GRCh37"
 
 def main():
-    sigSim.SigProfilerSimulator(project, project_path, genome, contexts=["96"], simulations=200, chrom_based=True,vcf=True)
+    sigSim.SigProfilerSimulator(project, path, genome, contexts=["96"], simulations=200, chrom_based=True,vcf=True)
 if __name__ == '__main__':
         main()
 
 def main():
-    hp.analysis(project, genome, contexts="96", simContext=["96"], project_path, subClassify=True, standardVC=True, TCGA=False, sanger=False, probability=True)
+    hp.analysis(project, genome, contexts="96", simContext=["96"], path, subClassify=True, standardVC=True, TCGA=False, sanger=False, probability=True)
 if __name__ == '__main__':
         main()
