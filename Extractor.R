c588769a73e0595c4ae941897418ee1f62634721
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2023-05-10 11:11:21 +0200

    Delete Extractor.R

--- Extractor.R
+++
@@ -1,9 +0,0 @@
-library(reticulate)
-use_python("/home/dnaro/.conda/envs/torchcuda/bin/python")
-library(SigProfilerMatrixGeneratorR)
-
-args=commandArgs(trailingOnly=TRUE)
-inputfolder=as.character(args[1])
-
-matrices <- SigProfilerMatrixGeneratorR("Test", "GRCh38", inputfolder, plot=T, exome=F,
-                                        bed_file=NULL, chrom_based=F, tsb_stat=F, seqInfo=F, cushion=100)
