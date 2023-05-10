a638b65f93a97a0cf3b31b3039976b02d07c7bec
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2023-05-10 10:57:16 +0200

    Update Extractor.py

--- Extractor.py
+++ Extractor.py
@@ -3,14 +3,17 @@ import os
 import sys
 from SigProfilerExtractor import sigpro as sig
 
+project = sys.argv[1]
+genome = sys.argv[2]
+output = sys.argv[3]
+
+from SigProfilerMatrixGenerator.scripts import SigProfilerMatrixGeneratorFunc as matGen
 #def main_function():
- #     sig.sigProfilerExtractor("vcf", "./Signatures", "./VCFs/", reference_genome="GRCh37", opportunity_genome = "GRCh37", context_type = "96", exome = False, 
- #                              minimum_signatures=1, maximum_signatures=2, nmf_replicates=100, resample = True, batch_size=1, cpu=1)
+#     matrices = matGen.SigProfilerMatrixGeneratorFunc("closer", "GRCh37", "./VCFs/",plot=False, exome=False, bed_file=None, chrom_based=False, tsb_stat=False, seqInfo=False, cushion=100)
 #if __name__=="__main__":
 #   main_function()
 
-from SigProfilerMatrixGenerator.scripts import SigProfilerMatrixGeneratorFunc as matGen
 def main_function():
-     matrices = matGen.SigProfilerMatrixGeneratorFunc("closer", "GRCh37", "./VCFs/",plot=False, exome=False, bed_file=None, chrom_based=False, tsb_stat=False, seqInfo=False, cushion=100)
+     matrices = matGen.SigProfilerMatrixGeneratorFunc(project, genome, output,plot=False, exome=False, bed_file=None, chrom_based=False, tsb_stat=False, seqInfo=False, cushion=100)
 if __name__=="__main__":
    main_function()
