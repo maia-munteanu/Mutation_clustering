c4cdd0f349193c28e1bbad54be4ef8f33f3ca31c
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2023-05-10 16:35:06 +0200

    Update MatrixGenerator.py

--- MatrixGenerator.py
+++ MatrixGenerator.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 import os
 import sys
-from SigProfilerExtractor import sigpro as sig
+from SigProfilerMatrixGenerator.scripts import SigProfilerMatrixGeneratorFunc as matGen
 
 project = sys.argv[1]
 genome = sys.argv[2]
 output = sys.argv[3]
-
-from SigProfilerMatrixGenerator.scripts import SigProfilerMatrixGeneratorFunc as matGen
-#def main_function():
-#     matrices = matGen.SigProfilerMatrixGeneratorFunc("closer", "GRCh37", "./VCFs/",plot=False, exome=False, bed_file=None, chrom_based=False, tsb_stat=False, seqInfo=False, cushion=100)
-#if __name__=="__main__":
-#   main_function()
 
 def main_function():
      matrices = matGen.SigProfilerMatrixGeneratorFunc(project, genome, output, plot=False, exome=False, bed_file=None, chrom_based=False, tsb_stat=False, seqInfo=False, cushion=100)
