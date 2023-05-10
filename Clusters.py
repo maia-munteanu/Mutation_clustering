a8734bf4db75de57486829f35c853eef0f0b884b
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2023-05-10 11:55:01 +0200

    Create Clusters.py

---
+++ Clusters.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os
+import sys
+from SigProfilerSimulator import SigProfilerSimulator as sigSim
+from SigProfilerClusters import SigProfilerClusters as hp
+
+project="Test"
+project_path="./VCFs/"
+genome="GRCh37"
+
+def main():
+    sigSim.SigProfilerSimulator(project, project_path, genome, contexts=["96"], simulations=200, chrom_based=True,vcf=True)
+if __name__ == '__main__':
+        main()
+
+def main():
+    hp.analysis(project, genome, contexts="96", simContext=["96"], project_path, subClassify=True, standardVC=True, TCGA=False, sanger=False, probability=True)
+if __name__ == '__main__':
+        main()
