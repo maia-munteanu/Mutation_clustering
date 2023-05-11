b17432c12c47e3e9e24b12e2a40907eee9559c5f
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2023-05-11 14:20:29 +0200

    Update SignatureExtractor.py

--- SignatureExtractor.py
+++ SignatureExtractor.py
@@ -3,12 +3,6 @@ import os
 import sys
 from SigProfilerExtractor import sigpro as sig
 
-#project = sys.argv[1]
-#matrix = sys.argv[2]
-#genome = sys.argv[3]
-#minsig = sys.argv[4]
-#maxsig = sys.argv[5]
-
 def main_function():
       sig.sigProfilerExtractor("matrix", "./Unclustered/Signatures", "./Unclustered/unclustered.SBS96.all", reference_genome="GRCh37", minimum_signatures=1, maximum_signatures=5, nmf_replicates=100, cpu=1)
 if __name__=="__main__":
