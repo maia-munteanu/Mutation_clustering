7da61cfcd6826e3034ac1b19af47533cfd16f84a
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2023-05-15 13:51:12 +0200

    Update SignatureExtractor.R

--- SignatureExtractor.R
+++ SignatureExtractor.R
@@ -2,8 +2,8 @@ library(reticulate)
 use_python("/usr/bin/python3")
 library(SigProfilerExtractorR)
 
-sigprofilerextractor(input_type = "matrix",
-                     output = "./Unclustered/Signatures",
-                     input_data = "./Unclustered/unclustered.SBS96.all", reference_genome="GRCh37", opportunity_genome="GRCh37",
+sigprofilerextractor(input_type = "vcf",
+                     output = "./unclustered_VCFs/Signatures",
+                     input_data = "./unclustered_VCFs/", reference_genome="GRCh37", opportunity_genome="GRCh37",
                      minimum_signatures=1, maximum_signatures=3, nmf_replicates=100, cpu=1, gpu=F, batch_size=1,make_decomposition_plots=F)
                      
