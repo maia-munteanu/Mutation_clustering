a529b433052015aedf30c3a9e36d4bef23fd1f05
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2023-05-31 12:21:15 +0200

    Update tsv_to_vcf.R

--- tsv_to_vcf.R
+++ tsv_to_vcf.R
@@ -5,13 +5,13 @@ sample=args[1]
 niter=args[2]
 otsv=args[3]
 rtsv=args[4]
+output=args[5]
 
 chromosomes=c(1:22,"X","Y")
 
 original_tsv=read.table(file = otsv, sep = '\t', header = FALSE)
 colnames(original_tsv)=c("chr","start","end","ref","alt","strand","sample")
 random_tsv=read.table(file = rtsv, sep = '\t', header = TRUE)
-file=paste0(sample,".snv.filt.random.R",niter,".vcf")
 
 vcf=data.frame(CHROM = c(), POS = c(), ID=c(), REF = c(), ALT = c(),  QUAL=c(), FILTER=c(), INFO=c())
 if(identical(original_tsv[,c(1,3,4,5)],random_tsv[,c(1,3,6,7)])){ #check that chr, pos, ref and alt fields for the original variants are the same
@@ -19,6 +19,6 @@ if(identical(original_tsv[,c(1,3,4,5)],random_tsv[,c(1,3,6,7)])){ #check that ch
       vcf$CHROM=factor(vcf$CHROM,levels=chromosomes[chromosomes %in% unique(vcf$CHROM)])
       vcf=vcf[order(vcf$CHROM,vcf$POS ),]
       colnames(vcf)[which(grepl("CHROM", colnames(vcf)))] = "#CHROM"
-      write.table(c("##fileformat=VCFv4.2"), file, row.names = F,col.names = F,quote = F)
-      fwrite(vcf, file, row.names = F,col.names = T,quote = F,append = T,sep="\t")
+      write.table(c("##fileformat=VCFv4.2"), output, row.names = F,col.names = F,quote = F)
+      fwrite(vcf, output, row.names = F,col.names = T,quote = F,append = T,sep="\t")
 }else{write("The observed variant information found in the randomised tsv file do not match the original variant information. Please check the output of randommut.", stderr())}
