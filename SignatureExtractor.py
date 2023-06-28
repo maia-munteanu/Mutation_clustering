179f6ed8ecc7d83ae02af445d82f59b59d3ce337
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2023-06-28 15:01:28 +0200

    Update SignatureExtractor.py

--- SignatureExtractor.py
+++ SignatureExtractor.py
@@ -8,9 +8,10 @@ matrix = sys.argv[2]
 genome = sys.argv[3]
 minsig = int(sys.argv[4])
 maxsig = int(sys.argv[5])
+cores = int(sys.argv[6])
 
 def main_function():
-      sig.sigProfilerExtractor("matrix", project, matrix, reference_genome=genome, minimum_signatures=minsig, maximum_signatures=maxsig, nmf_replicates=100, cpu=8)
+      sig.sigProfilerExtractor("matrix", project, matrix, reference_genome=genome, minimum_signatures=minsig, maximum_signatures=maxsig, nmf_replicates=100, cpu=cores)
 if __name__=="__main__":
    main_function()
 
